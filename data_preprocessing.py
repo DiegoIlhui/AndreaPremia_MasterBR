d5c0aa3dab139cff4607868a92e56110a5dc2908
Diego Barrios <diego.barrios@adv.loc>
2025-03-13 12:36:23 -0600

    Cambio de definición de ganadoras, si son estrellas deben de tener joyas ganadas para que sean ganadoras, pero si no tienen joyas y el perfil es mayorista

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -81,7 +81,7 @@ def procesar_reporte_general_de_usuarios(path):
   dtypes_validation = validate_dtypes(RGU.dtypes, dtype_dict, dates)
   assert dtypes_validation[0], dtypes_validation[1]
 
-  RGU["Ganadoras"] = RGU["JOYAS_TOTALES_GANADAS"].apply(lambda x: "No ganadora" if x==0 else "Ganadora")
+  RGU["Ganadoras"] = RGU.apply(lambda x: "No ganadora" if (x["JOYAS_TOTALES_GANADAS"]==0 and x["PERFIL"]=="Estrella") else "Ganadora", axis=1)
   RGU["Con canje"] = RGU["JOYAS_CANJEADOS"].apply(lambda x: "Sin canje" if x==0 else "Con canje")
   RGU["Con ingreso"] = [np.nan] * RGU.shape[0]
   RGU.loc[pd.isnull(RGU["ULTIMO_INGRESO_APP"]), "Con ingreso"] = "Sin ingreso"

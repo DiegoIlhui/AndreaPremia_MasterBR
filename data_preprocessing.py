85ae734c603f8e93c992e24d8270894829eae1a7
Diego Barrios <diego.barrios@adv.loc>
2025-04-03 16:45:30 -0600

    Corrección en el assert del cruce de tablas.

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -443,7 +443,7 @@ def procesar_datos(reporte_general_de_usuarios, reporte_de_metas_y_resultados, r
 def cruzar( dataframe1, dataframe2, col_tabla_izquierda, col_tabla_derecha, sufijos=None, metodo_cruce="ambas" ):
     how_options =  ["ambas","izquierda","derecha","izq-der"]
     how_dict = {"ambas":"inner","izquierda":"left","derecha":"right","izq-der":"outer"}
-    assert (metodo_cruce is in how_options), "El parámetro 'metodo_cruce' debe coincidir con alguna de las siguientes opciones: \"ambas\",\"izquierda\",\"derecha\",\"izq-der\"."
+    assert (metodo_cruce is in how_options), "El parámetro 'metodo_cruce' debe coincidir con alguna de las siguientes opciones: 'ambas','izquierda','derecha','izq-der'."
     if sufijos is not None:
         return dataframe1.merge( dataframe2, left_on=col_tabla_izquierda, right_on=col_tabla_derecha, suffixes=sufijos, how=how_dict[metodo_cruce] )
     else:
